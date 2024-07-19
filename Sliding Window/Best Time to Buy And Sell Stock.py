5cf27ce10e20e9ac024bfb153411ffd525e1c706
chenlianGGX <lian.chen@gogox.com>
2024-07-19 17:35:41 +0800

    Add unittest

--- Sliding Window/Best Time to Buy And Sell Stock.py
+++ Sliding Window/Best Time to Buy And Sell Stock.py
@@ -1,3 +1,5 @@
+from typing import List
+import unittest
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         res = 0
@@ -8,3 +10,17 @@ class Solution:
                 lowest = price
             res = max(res, price - lowest)
         return res
+
+
+
+
+# unittest for Best Time to Buy and Sell Stock
+class TestMaxProfit(unittest.TestCase):
+    def test_maxProfit(self):
+        self.assertEqual(Solution().maxProfit([7,1,5,3,6,4]), 5)
+        self.assertEqual(Solution().maxgitProfit([7,6,4,3,1]), 0)
+
+
+if __name__ == "__main__":
+    # execute unit test
+    unittest.main()
